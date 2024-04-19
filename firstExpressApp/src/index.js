14d893630a629f9aecc323a0b689d33055f6f7d7
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-04-19 20:52:06 +0530

    first express lec

--- firstExpressApp/src/index.js
+++ firstExpressApp/src/index.js
@@ -1,9 +1,16 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 //when we call the function express we create a new express server object
 const app = express();//http server object
 
 const PORT = 3000;
+
+
+app.use(bodyParser.json()); //now this is will act as middleware for all the request
+//now every request body can be converted to json // support for json give
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded())
 
 function m1(req, res, next){
 
@@ -28,6 +35,7 @@ function m2(req, res, next){
 
 app.get('/home', m1, m2, (req, res)=>{
 
+    console.log(req.url," ", req.query)
     console.log("/home called");
     return res.json ({msg:'ok'});
 })
@@ -36,3 +44,36 @@ app.listen(PORT, ()=>{
 
     console.log(`Server is listening on PORT ${PORT}`)
 })
+
+app.get('/product/:id/rating/:rating', (req, res)=>{
+   //:id is variable and product is static
+   //:id part is your url params and overall these kinds of routes are called as dynamic routes
+    console.log(req.params);
+    const pid = req.params.id;
+    const rating = req.params.rating;
+    return res.json(
+        {
+            productId : pid,
+            rating: rating
+        })
+})
+
+app.post('/data', (req, res)=>{
+
+    console.log(req.body)
+    return res.json({msg:"ok"});
+})
+
+//How client can send data to server
+/*
+1.URL Params -->
+                /product/7
+
+2.Request Body -->
+                can send sensitive info
+
+3.Query Params -->
+                ?key1=value1&key2=value2&key3=value3 
+                use res.query to get these params
+
+*/
